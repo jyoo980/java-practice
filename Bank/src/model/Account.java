646dda1bd8546850ee37e6216c80588948f188fa
James Yoo <jyoo980@gmail.com>
2018-04-28 14:00:35 -0700

    Added Bank solution

---
+++ Bank/src/model/Account.java
@@ -0,0 +1,70 @@
+package model;
+
+public class Account {
+
+    protected String owner;
+    protected Bank bank;
+    protected double chequingBalance;
+    protected double savingsBalance;
+    protected double debitLimit;
+
+    public Account(Bank bank, String owner) {
+        this.bank = bank;
+        chequingBalance = 0.0;
+        this.owner = owner;
+        savingsBalance = 0.0;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public double getChequingBalance() {
+        return chequingBalance;
+    }
+
+    public double getSavingsBalance() {
+        return savingsBalance;
+    }
+
+    public double getDebitLimit() {
+        return debitLimit;
+    }
+
+    public boolean depositIntoChequing(int amount) {
+        if (amount > 0) {
+            chequingBalance += amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean depositIntoSavings(int amount) {
+        if (amount > 0) {
+            savingsBalance += amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean debitPurchase(int amount) {
+        if (chequingBalance - amount >= 0) {
+            chequingBalance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public void registerBank(Bank bank) {
+        if (this.bank == null) {
+            this.bank = bank;
+        }
+    }
+}
--- Bank/src/model/RegularAccount.java
+++ Bank/src/model/RegularAccount.java
@@ -1,6 +1,6 @@
 package model;
 
-public class RegularAccount {
+public class RegularAccount extends Account {
 
     private static final double INITAL_DEBIT_LIMIT = 1500.00;
 
@@ -11,64 +11,8 @@ public class RegularAccount {
     private double debitLimit;
 
     public RegularAccount(String owner, Bank bank) {
-        this.owner = owner;
-        this.bank = bank;
-        bank.registerRegularAccount(this);
-        chequingBalance = 0.0;
-        savingsBalance = 0.0;
-        debitLimit = INITAL_DEBIT_LIMIT;
+       super(bank, owner);
+       this.debitLimit = INITAL_DEBIT_LIMIT;
     }
 
-    public String getOwner() {
-        return owner;
-    }
-
-    public double getChequingBalance() {
-        return chequingBalance;
-    }
-
-    public double getSavingsBalance() {
-        return savingsBalance;
-    }
-
-    public double getDebitLimit() {
-        return debitLimit;
-    }
-
-    public boolean depositIntoChequing(int amount) {
-        if (amount > 0) {
-            chequingBalance += amount;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean depositIntoSavings(int amount) {
-        if (amount > 0) {
-            savingsBalance += amount;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean debitPurchase(int amount) {
-        if (chequingBalance - amount >= 0) {
-            chequingBalance -= amount;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public Bank getBank() {
-        return bank;
-    }
-
-    public void registerBank(Bank bank) {
-        if (this.bank == null) {
-            this.bank = bank;
-        }
-    }
 }
